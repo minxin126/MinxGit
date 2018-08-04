0ab278fbf05e9ebe47117e71ab7c1685119897a9
minxin126 <wangminxin6@126.com>
2018-08-04 17:20:07 +0800

    remember somethings

--- gitpull.sh
+++ gitpull.sh
@@ -3,7 +3,7 @@ git config --global user.email "wangminxin6@126.com"
 
 git config --global user.name "minxin126"
 
-git clone https://github.com/minxin126/minxgit.git
+#git clone https://github.com/minxin126/minxgit.git
 #git add .
 #git commit -a -m ""
 sudo git pull origin master
